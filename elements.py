01a498d686d1828b230d22471f3493ef3dd6dac6
Geovani Lopes Dias <geovani.phy.dias@gmail.com>
2024-05-02 16:00:53 -0300

    Update elements.py

--- elements.py
+++ elements.py
@@ -11,17 +11,63 @@ class Element():
         self.electronegativity = e
 
 
+    @staticmethod
+    def set_index(number):
+        if type(number) is not int:
+            raise TypeError('Número de átomos deve ser um inteiro')
+        if number < 1:
+            raise ValueError('Número de átomos deve ser superior à 1')
+        
+        UNICODE_ALGARISMS = [u'\u2080', u'\u2081', u'\u2082', u'\u2083', u'\u2084', u'\u2085' , u'\u2086', u'\u2087', '\u2088', '\u2089']
+        return ''.join([UNICODE_ALGARISMS[int(digit)] for digit in str(number)]) if number > 1 else ''
+
+
+    @staticmethod
+    def set_molar_mass(atoms):
+        """
+        Given a list of dictionaries contaning an Element instance at the key 'element' and
+        its number of atoms in the key 'quantity', returns the molecular/ionic molar mass.
+        """
+        if type(atoms) is not list:
+            raise TypeError
+        try:
+            return sum(atom["element"].get_molar_mass() * atom["quantity"] for atom in atoms)
+        except TypeError as e:
+            print(f'{e}: values of the dicts in list do not match the proper type.')
+
+
+    @staticmethod
+    def get_element_index(element, substance):
+        """
+        Given a list of dictionaries contaning an Element instance at the key 'element' and
+        its number of atoms in the key 'quantity', returns the quantity value of the given element.
+        """
+        if not isinstance(element, PeriodicTable) or type(substance) is not list:
+            raise TypeError(f'Element is {type(element)} (should be PeriodicTable) or substance is {type(substance)} should be list.')
+
+        index = next(filter(lambda atom: atom["element"] == element, substance), None)
+        return index["quantity"] if index is not None else 0
+
+
+    @staticmethod
+    def set_formula(atoms):
+        return ''.join([f'{atom["element"].get_symbol()}{Element.set_index(atom["quantity"])}' for atom in atoms])
+
+
 
 class PeriodicTable(Enum):
     # Alkalines
     LITHIUM = Element('lítio', 'Li', 3, 6.9, 1.0)
-    BERILIUM = Element('berílio', 'Be', 4, 9.0, 1.5)
+    BERILLIUM = Element('berílio', 'Be', 4, 9.0, 1.5)
     SODIUM = Element('sódio', 'Na', 11, 23.0, .9)
     MAGNESIUM = Element('magnésio', 'Mg', 12, 24.3, 1.2)
     POTASSIUM = Element('potássio', 'K', 19, 39.1, .8) 
     CALCIUM = Element('cálcio', 'Ca', 20, 40.1, 1.0)
+    RUBIDIUM = Element('rubídio', 'Rb', 37, 85.5, .8)
+    STRONTIUM = Element('estrôncio', 'Sr', 38, 87.6, 1.0)
     CESIUM = Element('césio', 'Cs', 55, 132.9, .7)
     BARIUM = Element('bário', 'Ba', 56, 137.3, .9)
+    RADIUM = Element('rádio', 'Ra', 88, 226.0, .9)
 
     # Non-metals
     HYDROGEN = Element('hidrogênio', 'H', 1, 1.0, 2.1)
@@ -53,15 +99,21 @@ class PeriodicTable(Enum):
     COPPER = Element('zinco', 'Zn', 30, 65.4, 1.6)
     ZINC = Element('zinco', 'Zn', 30, 65.4, 1.6)
     SILVER = Element('prata', 'Ag', 47, 107.9, 1.9)
+    CADMIUM = Element('cádmio', 'Cd', 48, 112.4, 1.7)
     TIN = Element('estanho', 'Sn', 50, 118.6, 1.8)
-    PLATINE = Element('platina', 'Pt', 78, 195.0, 2.2)
+    PLATINUM = Element('platina', 'Pt', 78, 195.0, 2.2)
     GOLD = Element('ouro', 'Au', 79, 197.0, 2.4)
     QUICKSILVER = Element('mercúrio', 'Hg', 80, 200.5, 1.9)
     LEAD = Element('chumbo', 'Pb', 82, 207.2, 1.8)
+    BISMUTH = Element('bismuto', 'Bi', 83, 207, 1.9)
 
 
     def get_symbol(self):
         return self.value.symbol
+
+
+    def get_name(self):
+        return self.value.name
 
 
     def get_molar_mass(self):
